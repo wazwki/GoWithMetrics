85bf597f5c44fe0df97b3fb1ae854ec2bbb82d18
wazwki <vazkildi@gmail.com>
2024-09-13 12:02:05 +0300

    add code example

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "handler"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(requestCount)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestCount.With(prometheus.Labels{"method": r.Method, "handler": "/"})
+		w.Write([]byte("Hello, Prometheus!"))
+	})
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	http.ListenAndServe(":8080", nil)
 }
